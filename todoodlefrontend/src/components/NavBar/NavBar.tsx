084246d86d9c90cd83cedb6c312d91a22b8917eb
john <ypoh007@e.ntu.edu.sg>
2025-08-09 22:14:37 +0800

    Created NavBar

---
+++ todoodlefrontend/src/components/NavBar/NavBar.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import '../../styles/globals.css'
+import './NavBar.css'
+
+const NavBar = () => {
+    return (
+        <>
+            <nav className="navbar">
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+                <Link to="/sign_up">Sign Up</Link>
+                <Link to="/sign_in">Sign In</Link>
+            </nav>
+        </>
+    )
+}
+
+export default NavBar
