607d97694d15677755d23a088e4466af0f3b4092
john <ypoh007@e.ntu.edu.sg>
2025-11-01 21:30:10 +0800

    test config

--- backend/index.js
+++ backend/index.js
@@ -25,17 +25,27 @@ app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-	proxy: true,
     cookie: {
         secure: true,
         maxAge: 24 * 60 * 60 * 1000,
-		httpOnly: true,
-		sameSite: 'none'
     }
 }))
 
 app.use(passport.initialize())
 app.use(passport.session())
+
+app.use((req, res, next) => {
+    if (req.user && !req.cookies.accessToken) {
+        res.cookie('accessToken', req.user.accessToken, {
+            httpOnly: true,
+            secure: true,
+            sameSite: 'none',
+            maxAge: 24 * 60 * 60 * 1000,
+            path: '/'
+        })
+    }
+    next()
+})
 
 app.use("/api/v1/folder", folderRoutes)
 app.use("/api/v1/note", noteRoutes)
